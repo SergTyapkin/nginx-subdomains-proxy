f960f216ae59b93d8f578fa4c21b115e8b428234
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-03-23 21:33:42 +0300

    fix networks

--- docker-deploy/scripts/generate-docker-compose.sh
+++ docker-deploy/scripts/generate-docker-compose.sh
@@ -1,14 +1,19 @@
 cat ./docker-deploy/docker-compose.template.yaml > ./docker-deploy/docker-compose.yaml
-final_networks_string="networks:
+default_proxy_network_name="$(. "docker-deploy/.env"; eval "echo \${DEFAULT_PROXY_NETWORK_NAME} | tr -dc \"a-zA-Z._-\"" )"
+final_networks_string="      - ${default_proxy_network_name}
+networks:
   default:
+  ${default_proxy_network_name}:
+    name: ${default_proxy_network_name}
 "
-proxy_services_count=$(. "docker-deploy/.env"; echo "$PROXY_SERVICES_COUNT" | tr -dc [0-9])
+proxy_services_count=$(. "docker-deploy/.env"; echo "$PROXY_SERVICES_COUNT" | tr -dc "0-9")
 for proxy_num in $(seq 1 $proxy_services_count); do
   # Add "networks" to docker-compose.yaml
   proxy_network_var_name="PROXY_${proxy_num}_NETWORK_NAME"
-  proxy_network_name=$(. "docker-deploy/.env"; eval "echo \${${proxy_network_var_name}} | tr -dc [a-zA-Z\-\_\.]" )
+  proxy_network_name=$(. "docker-deploy/.env"; eval "echo \${${proxy_network_var_name}} | tr -dc \"a-zA-Z._-\"" )
   echo "      - ${proxy_network_name}" >> ./docker-deploy/docker-compose.yaml;
   final_networks_string="${final_networks_string}  ${proxy_network_name}:
+    name: ${proxy_network_name}
 "
 done
 echo "${final_networks_string}" >> ./docker-deploy/docker-compose.yaml
