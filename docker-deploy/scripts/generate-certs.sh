df4e139f7a294e4ad3d1e44ada7558ef1edacf22
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-03-23 20:46:47 +0300

    add certificates generator

--- docker-deploy/scripts/generate-certs.sh
+++ docker-deploy/scripts/generate-certs.sh
@@ -1,5 +1,9 @@
 cd docker-deploy || exit
 docker compose down
 docker compose up -d nginx-certbot
-docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d "$(DOMAIN_URL)"
+STR="docker compose run --rm certbot certonly --webroot --webroot-path /var/www/certbot/ -d ${DOMAIN_URL}"
+for proxy_num in $(seq 1 "$(. "docker-deploy/.env"; eval "echo \${PROXY_SERVICES_COUNT}" | tr -dc [0-9])"); do
+  STR="${STR} -d $(. "docker-deploy/.env"; eval "echo \${PROXY_${proxy_num}_SUBDOMAIN}" | tr -dc [a-zA-Z0-9\_\.\-]).${DOMAIN_URL}"
+done
+eval "${STR}"
 sudo chmod ugo+rwx -R ./certbot/
